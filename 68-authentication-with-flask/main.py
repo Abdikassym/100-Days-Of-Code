a9aa70475bc50ccef0ea593dbfdd1963960acd27
Abdikassym <79089741+Abdikassym@users.noreply.github.com>
2024-05-19 13:32:20 +0500

    Add files via upload

---
+++ 68-authentication-with-flask/main.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+
+# CREATE TABLE IN DB
+class User(db.Model, UserMixin):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(1000))
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        user = db.session.execute(db.select(User).where(User.email == request.form.get('email'))).scalar()
+        if user:
+            flash("You have already signed in with that email, please try log in instead")
+            return redirect(url_for('login'))
+        else:
+            password = request.form.get("password")
+            hashed_password = generate_password_hash(password=password, method="pbkdf2:sha256", salt_length=8)
+            new_user = User(
+                name=request.form.get("name"),
+                email=request.form.get("email"),
+                password=hashed_password
+                    )
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+
+            return redirect(url_for('secrets'))
+    return render_template("register.html")
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user)
+                return redirect(url_for('secrets'))
+            else:
+                flash("Incorrect password, please try again.")
+        else:
+            flash("This email does not exist, please try again.")
+    return render_template("login.html")
+
+
+@app.route('/secrets')
+@login_required
+def secrets():
+    print(current_user.name)
+    return render_template("secrets.html")
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/download')
+@login_required
+def download():
+    return send_from_directory('static', path="files/cheat_sheet.pdf")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
