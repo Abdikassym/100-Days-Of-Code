35c5637e72dd16b4d4614376f113d63a3b419797
Abdikassym <79089741+Abdikassym@users.noreply.github.com>
2024-06-01 14:58:30 +0500

    Add files via upload

---
+++ 24-files-directories-paths/files.py
@@ -0,0 +1,9 @@
+# with open('my_file.txt') as file:
+#     print(file.read())
+#
+# with open('my_file.txt', mode='w') as file:
+#     file.write('New text.')
+
+with open('new_file.txt', mode='w') as file:
+    file.write('New text in new file')
+
