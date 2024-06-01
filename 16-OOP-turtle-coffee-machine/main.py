0b1d0444f60237fa493a7fad5daa5cddfdf3ea7a
Abdikassym <79089741+Abdikassym@users.noreply.github.com>
2024-06-01 14:53:29 +0500

    Add files via upload

---
+++ 16-OOP-turtle-coffee-machine/main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("DeepSkyBlue")
+# timmy.forward(100)
+# timmy.goto(250, 250)
+# timmy.goto(-250, 150)
+#
+# my_screen = Screen()
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Charmander", "Squirtle"])
+# table.add_column("Type", ["Electric", "Fire", "Water"])
+# table.align = 'l'
+#
+# print(table)
+
